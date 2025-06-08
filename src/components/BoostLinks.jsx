5e730b7ef3ae3b80a0dcfbc485b15aa0a181124d
Joseph Afolabi <maverickoluwatomisin@gmail.com>
2025-06-08 18:01:03 +0100

    bg-boost

--- src/components/BoostLinks.jsx
+++ src/components/BoostLinks.jsx
@@ -1,6 +1,9 @@
 const BoostLinks = () => {
 	return (
-		<div className="w-full h-[300px] flex flex-col justify-center items-center bg-myPurple2 ">
+		<div
+			id="boost-link-section"
+			className="w-full h-[300px] flex flex-col justify-center items-center bg-myPurple2 bg-[url('/images/bg-boost-mobile.svg')] bg-no-repeat bg-left-top bg-cover "
+		>
 			<h2 className="font-bold text-[28px] leading-[48px] text-center tracking-[-0.7px] text-white ">
 				Boost your links today
 			</h2>
